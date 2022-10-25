7e74022805ff75578bbca9777d47e30e5c9bfd1d
wwwonwo <wwwonwo@gmail.com>
2022-10-25 10:11:33 +0900

    create naming folder

---
+++ dashboard/jun_young/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JunYoungConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "jun_young"
---
+++ dashboard/won_woo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WonWooConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "won_woo"
